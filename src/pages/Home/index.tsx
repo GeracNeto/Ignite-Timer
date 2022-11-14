e4b874b72be503d3962477371143711fb0ffc369
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-11-14 15:06:36 -0300

    Criada estrutura da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,32 @@
+import { Play } from "phosphor-react";
+
 export function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <form>
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
     </div>
   );
 }
